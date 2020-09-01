caa6dc7953288655b9db5231f4aee3672839c43c
LesliePratt <leslie.pratt@g.austincc.edu>
2020-09-01 11:10:30 -0500

    Fixed missing code in functions.h due to wrong dir in DevC++

--- functions.h
+++ functions.h
@@ -1,1 +1,18 @@
+/************************************
+Name: Leslie Pratt
 
+Coding: 01
+
+Purpose: This is a functions header file 
+		 my Hello-World program. All the 
+		 necessary function headers will 
+		 be added here.
+*************************************/
+
+#ifndef HELLOWORLD_FUNCTIONS_H
+#define HELLOWORLD_FUNCTIONS_H
+
+int string_length(const char*);
+//int string_length(const char *str);
+
+#endif //HELLOWORLD_FUNCTIONS_H
